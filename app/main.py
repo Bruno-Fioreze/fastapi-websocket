3c39bb7644a6a2043760c388841602de12fc50bd
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2023-05-07 06:21:05 -0300

    return data in first connection

--- app/main.py
+++ app/main.py
@@ -5,18 +5,32 @@ app = FastAPI()
 templates = Jinja2Templates(directory="./templates/")
 
 websockets = [] 
+messages = []
 
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.websocket("/ws") 
+async def broadcast(message: str):
+    for websocket in app.websockets:
+        await websocket.send_text(message)
+
+@app.websocket("/ws/")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+    try:
+        app.websockets.add(websocket)
 
-    websockets.append(websocket)
+        for message in messages:
+            await websocket.send_text(message)
 
-    while True:
-        data = await websocket.receive_text()
-        for ws in websockets:
-            await ws.send_text(f"Message text was: {data}")
+        while True:
+            data = await websocket.receive_text()
+            messages.append(data)
+
+            await broadcast(data)
+
+    except Exception as e:
+        print(f"WebSocket error: {e}")
+
+    finally:
+        app.websockets.remove(websocket)
